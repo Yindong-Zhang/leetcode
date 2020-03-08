e9210d2b1790101f3305f10fdcbaa858b1871a49
yindong zhang <so_go_flying@163.com>
2020-03-08 11:27:26 +0800

    2020/03/08

--- main.cpp
+++ main.cpp
@@ -3,25 +3,16 @@
 #include<vector>
 #include<array>
 #include"printVector.h"
-#include"medianInTwoSortedArray_4.h"
+#include"minCostTickets_983.h"
 using namespace std;
 
 
 
 int main()
 {
-    MedianInTwoSortedArray solution;
-//    vector<int> test{2,1,5,6,2,3};
-//    string s = "intention", p = "execution";
-//    cout << solution.F(15, test, 2) << endl;
-//    vector<vector<int>> test{
-//            { 1, 2, 3 },
-//            { 4, 5, 6 },
-//            { 7, 8, 9 }
-//    };
-//    string a = "123456789", b=  "987654321";
-    vector<int> a = {2, 3,}, b = {};
-    auto res = solution.findMedianSortedArrays(a, b);
+    MinCostTickets solution;
+    vector<int> days = {1,4,6,7,8,20}, costs = {2,7,15};
+    auto res = solution.mincostTickets(days, costs);
     cout << res << endl;
 //    for(auto vec : res){
 //        printVector(vec);
