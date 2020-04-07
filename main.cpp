46a6959de702e8c25d235ccf5b9d471ce706103b
yindong zhang <so_go_flying@163.com>
2020-04-07 17:35:06 +0800

    2020/04/07

--- main.cpp
+++ main.cpp
@@ -3,24 +3,27 @@
 #include<vector>
 #include<array>
 #include"printVector.h"
-#include"majorityElementII.h"
+#include"underGroundGame.h"
 using namespace std;
 
 
 
 int main()
 {
-    MajorityElementII solution;
+    Dungeon solution;
 //    vector<int> days = {1,4,6,7,8,20}, costs = {2,7,15};
-    vector<int> nums = {3, 2, 3};
+//    vector<int> nums = {3, 2, 3};
+    vector<vector<int>> a{{-2, -3, 3},
+                          {-5, -10, 1},
+                          {10, 30, -5}};
 //    int target = -100;
 //    string s= "abcde", p = "abcde";
 //    string s= "+e";
-    auto res = solution.majorityElement(nums);
+    auto res = solution.calculateMinimumHP(a);
 //    cout << res << endl;
 //    for(auto vec : res){
 //        printVector(vec);
 //    }
-    printVector(res);
-//    cout << "Result: " << res << endl;
+//    printVector(res);
+    cout << "Result: " << res << endl;
 }
